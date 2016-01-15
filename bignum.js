734da856123305cabefe16a213101d02652a2f29
Joe Farebrother <nintendo.joe.farebrother@gmail.com>
2016-01-15 20:36:14 +0000

    created comparison operators and a quick test page

--- bignum.js
+++ bignum.js
@@ -6,7 +6,7 @@ var Bignum = (function(){ //immediately involked
 		this.digits = digits;
 	}
 
-	function createFromNum(num){
+	function createFromNumber(num){
 		if(!num){
 			return Bignum(0, []);
 		}
@@ -18,9 +18,9 @@ var Bignum = (function(){ //immediately involked
 		}
 		else{sign = 1;}
 
-		digits = [];
+		var digits = [];
 		while(num !== 0){
-			digits.shift(num % MAX_INT);
+			digits.unshift(num % MAX_INT);
 			num = Math.floor(num / MAX_INT);
 		}
 		
@@ -29,9 +29,36 @@ var Bignum = (function(){ //immediately involked
 
 	function createFromString(str){
 		//idk how to implement this right now
-		throw new Error("Creation from string not yet supported");
+		throw new Error("Bignum creation from string not yet supported");
 	}
 
+	function create(from){
+		if(typeof from === "object"){return from;}
+		if(typeof from === "number"){return createFromNumber(from);}
+		if(typeof from === "string"){return createFromString(from);}
+		throw new Error("Unrecognised type for Bignum creation");
+	}
 
-	return createFromNum; //Temporary, will later make a function to decide between createFromNum and createFromString
+	Bignum.prototype.compare = function(b){ //returns 0 if a==b, +ve if a>b, -ve if a<b
+		var a = this;
+		b = create(b);
+		if(a.sign != b.sign){return a.sign - b.sign;}
+		return compareDigits(a.digits, b.digits)
+	};
+
+	function compareDigits(a, b){
+		if(a.length != b.length){return a.sign - b.sign;}
+		for (var i = 0; i < a.length; i++) {
+			if(a[i] != b[i]){return a[i] - b[i];}
+		};
+		return 0;
+	}
+	
+	Bignum.prototype.greaterThan        = function(b){return this.compare(b) > 0;};
+	Bignum.prototype.lessThan           = function(b){return this.compare(b) < 0;};
+	Bignum.prototype.equals             = function(b){return this.compare(b) == 0;};
+	Bignum.prototype.greaterThanOrEqual = function(b){return this.compare(b) >= 0;};
+	Bignum.prototype.lessThanOrEqual    = function(b){return this.compare(b) <= 0;};
+
+	return create; 
 })()
